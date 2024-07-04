3da4e69e58a4c2970f8a30edcf5dd148ca064195
bhavyasri89 <165119358+bhavyasri89@users.noreply.github.com>
2024-07-04 10:02:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ with open('Scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)
 
 
-
-def predict(bed,bath,size):
+def predict(Bedrooms,Bathrooms,size):
     
 
-    input_data=np.array([[bed,bath,size]])
+    input_data=np.array([[Bedrooms,Bathrooms,size]])
 
     input_df=scaler.transform(input_data)
 
@@ -25,12 +24,12 @@ if __name__ == '__main__':
 
     col1,col2 =st.columns([2,1])
 
-    bed =  col1.slider('No.Of Bedrooms', max_value=10,min_value=0,value=2)
-    bath= col1.slider('No.Of Bathrooms',max_value=10,min_value=0,value=2)
+    bed =  col1.number_input('No.Of Bedrooms', max_value=10,min_value=0,value=2)
+    bath= col1.number_input('No.Of Bathrooms',max_value=10,min_value=0,value=2)
     size= col1.number_input('Size in Sqft',max_value=10000,min_value=500,value=1000,step=250)
     
 
-    result=predict(bed,bath,size)
-    col2.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Fhouse-prices.html&psig=AOvVaw26TtZtqbewdU60ldIkEjl1&ust=1719818884565000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOiSsMrmgocDFQAAAAAdAAAAABAE',use_column_width=True)
+    result=predict(Bedrooms,Bathrooms,size)
+    col2.image('https://img.freepik.com/free-photo/blue-house-with-blue-roof-sky-background_1340-25953.jpg',use_column_width=True)
 
     col2.write(f'The Predictedmvalue is :{result} Lakhs')
